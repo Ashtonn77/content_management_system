ab2b33fe7b42e5013d292e33a71c0808177ae81c
Ashtonn77 <ashtonrbay@gmail.com>
2020-05-21 20:07:45 +0200

    added username check

--- Includes/functions.php
+++ Includes/functions.php
@@ -1,8 +1,21 @@
+<?php require_once("Includes/db.php"); ?>
 <?php
-function redirectTo($newLocation){
-    header('Location:'.$newLocation);
+function redirectTo($newLocation)
+{
+    header('Location:' . $newLocation);
     exit;
 }
-
-
-?>
+function checkIfUsernameExists($username)
+{
+    global $connectingDb;
+    $sql = "SELECT username FROM admins WHERE username=:Username";
+    $stmt = $connectingDb->prepare($sql);
+    $stmt->bindValue(':Username', $username);
+    $stmt->execute();
+    $result = $stmt->rowCount();
+    if ($result == 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
